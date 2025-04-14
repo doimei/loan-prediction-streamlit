591dea2f51e1d16c771b9f2ee002f66e774f4b41
doimei <50816704+doimei@users.noreply.github.com>
2025-04-13 19:19:35 -0700

    Add files via upload
    Initial upload of Streamlit app files

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle # to load a saved model
+import base64 # to open .gif files in streamlit app
+
+@st.cache(suppress_st_warning=True)
+def get_fvalue(val):
+	feature_dict={"No":1, "Yes":2}
+	for key.value in feature_dict.items():
+		if val == key:
+			return value
+
+def get_value(val,my_dict):
+	for key.value in my_dict.items():
+		if val == key:
+			return value
+app_mode=st.sidebar.selectbox('Select Page',['Home','Prediction']) #two pages
